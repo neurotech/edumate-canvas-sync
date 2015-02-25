0235ef44d5a17fc3c536f2a92eab2fc6fd68f0cb
Tim Douglas <neurotech@gmail.com>
2015-02-25 15:36:42 +1100

    Added happy basics

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+var Hapi = require('hapi');
 var request = require('request');
 var prettyjson = require('prettyjson');
 var config = require('./config');
+
+var server = new Hapi.Server();
+server.connection({ port: config.http.port });
 
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
@@ -12,3 +16,15 @@ function callback(error, response, body) {
 };
 
 request(config.requestOptions, callback);
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('There is nothing here.');
+  }
+});
+
+// server.start(function () {
+//   console.log('Server running at:', server.info.uri);
+// });
