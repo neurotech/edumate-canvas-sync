f0718c610090436060fd74ab324094599f0c5126
Tim Douglas <neurotech@gmail.com>
2015-03-18 15:06:32 +1100

    Added commander to app.js and tidied up

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+var program = require('commander');
 var fs = require('fs');
 var request = require('request');
 var schedule = require('node-schedule');
 var moment = require('moment');
+
 var config = require('./config');
 var canvas = require('./lib/canvas');
+ 
+program
+  .command('upload [csv]')
+  .description('Upload CSV as new SIS Import job.')
+  .action(function(csv){
+    canvas.sisUpload(csv);
+  });
 
-//canvas.sisUpload('staff');
-canvas.sisStatus();
+program
+  .command('status')
+  .description('Check Canvas SIS Import status.')
+  .action(function(){
+    canvas.sisStatus();
+  });
+
+program.parse(process.argv);
 
 // node-schedule:
-
+// --------------
 // schedule.scheduleJob(config.uploadSchedules.test, function(){
 //   console.log('Running staff upload at: ' + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 // });
