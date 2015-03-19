6111a6e250b266c735d74f70b5119233478c5c11
Tim Douglas <neurotech@gmail.com>
2015-03-19 21:14:54 +1100

    Refactoring app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+var program = require('commander');
+var Acho = require('acho');
+var schedule = require('node-schedule');
+var moment = require('moment');
+var canvas = require('./lib/canvas');
+var server = require('./lib/hapi');
+
+var acho = new Acho({color: true});
+
+// Command line support
+program
+  .command('serve')
+  .description('Start the hapi server.')
+  .action(function(){
+    server.start(function (err) {
+      if (err) {
+        throw err;
+      }
+      console.log('hapi server started');
+    });
+});
+
+program
+  .command('upload [csv]')
+  .description('Upload CSV as new SIS Import job.')
+  .action(function(csv){
+    canvas.sisUpload(csv)
+      .then(function(data) {
+        acho.success(data.status);
+      }, function(error) {
+        acho.error(error);
+      });
+});
+
+program
+  .command('status')
+  .description('Check latest Canvas SIS Import status.')
+  .action(function(){
+    canvas.sisStatus()
+      .then(function(data) {
+        console.log(data);
+      }, function(error) {
+        acho.error(error);
+    });
+});
+
+program.parse(process.argv);
+
+// node-schedule:
+// --------------
+// schedule.scheduleJob(config.uploadSchedules.test, function(){
+//   console.log('Running staff upload at: ' + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+// });
