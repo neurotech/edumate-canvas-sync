5dd5252a6a675a1c3bb3f8d0977c0d7ea066cc3c
Tim Douglas <neurotech@gmail.com>
2015-03-19 13:52:40 +1100

    Moved success message into sisUpload Promise

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var canvas = require('./lib/canvas');
 
 var acho = new Acho({color: true});
 
+// Command line support
 program
   .command('upload [csv]')
   .description('Upload CSV as new SIS Import job.')
   .action(function(csv){
     canvas.sisUpload(csv)
       .then(function(data) {
-        acho.success(data.csv + '.csv uploaded.');
-        acho.success('Import ID #' + data.canvas.id + ' started ' + moment(data.canvas.created_at).format("dddd, MMMM Do YYYY [at] hh:mm:ss A."));
+        acho.success(data.status);
       }, function(error) {
         acho.error(error);
     });
