7ac010a8f99bc85705043f2076f45bddfc08fe5c
Tim Douglas <neurotech@gmail.com>
2015-03-19 16:36:51 +1100

    Added more hapi routes, views support, command line option to start hapi

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ var acho = new Acho({color: true});
 // creating the hapi server instance
 var server = new hapi.Server();
 
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: './views'
+});
+
 // adding a new connection that can be listened on
 server.connection({
   port: config.http.port,
@@ -30,11 +37,43 @@ server.route({
 
 server.route({
   method: 'GET',
-  path: '/sis-status',
+  path: '/sis-status/json',
   handler: function (request, reply) {
     canvas.sisStatus()
       .then(function(data) {
         reply(data);
+      }, function(error) {
+        reply(error);
+      });
+  }
+});
+
+// /sis-status needs tidying:
+
+server.route({
+  method: 'GET',
+  path: '/sis-status',
+  handler: function (request, reply) {
+    canvas.sisStatus()
+      .then(function(data) {
+        var time = 'dddd, MMMM Do YYYY [at] hh:mm:ss A';
+        if (!data.ended_at) {
+          var ended = '-'
+        } else {
+          var ended = moment(data.ended_at).format(time);
+        }
+        var viewData = {
+          id: data.id,
+          created: moment(data.created_at).format(time),
+          started: moment(data.started_at).format(time),
+          ended: ended,
+          updated: moment(data.updated_at).format(time),
+          progress: data.progress,
+          status: data.workflow_state,
+          counts: data.data.counts,
+          diff: data.diffing_data_set_identifier
+        };
+        reply.view('sis-status', viewData);
       }, function(error) {
         reply(error);
       });
@@ -47,25 +86,29 @@ server.register({
   if (err) { acho.error('Failed to load plugin:', err); }
 });
 
-// starting the server
-server.start(function (err) {
-  if (err) {
-    throw err;
-  }
-  console.log('hapi server started');
-});
-
 // Command line support
 program
-.command('upload [csv]')
-.description('Upload CSV as new SIS Import job.')
-.action(function(csv){
-  canvas.sisUpload(csv)
-  .then(function(data) {
-    acho.success(data.status);
-  }, function(error) {
-    acho.error(error);
-  });
+  .command('serve')
+  .description('Start the hapi server.')
+  .action(function(){
+    server.start(function (err) {
+      if (err) {
+        throw err;
+      }
+      console.log('hapi server started');
+    });
+});
+
+program
+  .command('upload [csv]')
+  .description('Upload CSV as new SIS Import job.')
+  .action(function(csv){
+    canvas.sisUpload(csv)
+      .then(function(data) {
+        acho.success(data.status);
+      }, function(error) {
+        acho.error(error);
+      });
 });
 
 program
