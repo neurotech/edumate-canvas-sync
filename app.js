6f1517c508153dcbf21a4fcb4ba423f779c5658e
Tim Douglas <neurotech@gmail.com>
2015-03-29 16:54:10 +1100

    Commenting out nested query promise for now

--- app.js
+++ app.js
@@ -8,22 +8,23 @@ var program = require('./lib/cli');
 var edumate = require('./lib/db/edumate');
 var acho = new Acho({color: true});
 
+edumate.query('network-traffic', 'SELECT * FROM DB2INST1.NETWORK_TRAFFIC', 'csv');
 
-edumate.query('terms', 'SELECT * FROM DB2INST1.VIEW_CANVAS_TERMS', 'csv')
-  .then(function(res) {
-    acho.success(res);
-    // next query
-    setTimeout(function() {
-      edumate.query('terms', 'SELECT * FROM DB2INST1.VIEW_CANVAS_TERMS', 'cache')
-        .then(function(res) {
-          acho.success(res);
-        }, function(err) {
-          acho.error(err);
-        });
-    }, 1500);
-  }, function(err) {
-    acho.error(err);
-  });
+// edumate.query('terms', 'SELECT * FROM DB2INST1.VIEW_CANVAS_TERMS', 'csv')
+//   .then(function(res) {
+//     acho.success(res);
+//     // next query
+//     setTimeout(function() {
+//       edumate.query('terms', 'SELECT * FROM DB2INST1.VIEW_CANVAS_TERMS', 'cache')
+//         .then(function(res) {
+//           acho.success(res);
+//         }, function(err) {
+//           acho.error(err);
+//         });
+//     }, 1500);
+//   }, function(err) {
+//     acho.error(err);
+//   });
 
 program.parse(process.argv);
 
