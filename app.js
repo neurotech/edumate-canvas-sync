0417b27f0be23dcae6c1f753c5f127ad34cfafae
Tim Douglas <neurotech@gmail.com>
2015-03-29 21:51:31 +1100

    Cleaning up old comments in app.js

--- app.js
+++ app.js
@@ -11,26 +11,12 @@ var acho = new Acho({color: true});
 
 edumate.query(datasets.terms.name, datasets.terms.sql);
 
-// edumate.query('terms', 'SELECT * FROM DB2INST1.VIEW_CANVAS_TERMS', 'csv')
-//   .then(function(res) {
-//     acho.success(res);
-//     // next query
-//     setTimeout(function() {
-//       edumate.query('terms', 'SELECT * FROM DB2INST1.VIEW_CANVAS_TERMS', 'cache')
-//         .then(function(res) {
-//           acho.success(res);
-//         }, function(err) {
-//           acho.error(err);
-//         });
-//     }, 1500);
-//   }, function(err) {
-//     acho.error(err);
-//   });
-
 program.parse(process.argv);
 
-// node-schedule:
-// --------------
-// schedule.scheduleJob(config.uploadSchedules.test, function(){
-//   console.log('Running staff upload at: ' + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-// });
+/*
+node-schedule:
+--------------
+schedule.scheduleJob(config.uploadSchedules.test, function(){
+  console.log('Running staff upload at: ' + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+});
+*/
