71b936b356d8c9d384e2d8e256338b1cc81c6f4c
Tim Douglas <neurotech@gmail.com>
2015-03-30 15:28:15 +1100

    Added schedules to datasets module

--- datasets.js
+++ datasets.js
@@ -1,35 +1,42 @@
 'use strict';
 
+var schedule = require('node-schedule');
 var datasets = {};
 
 datasets.terms = {
   name: 'terms',
-  sql: 'SELECT * FROM DB2INST1.view_canvas_terms'
+  sql: 'SELECT * FROM DB2INST1.view_canvas_terms',
+  schedule: { hour: 18, minute: 0, dayOfWeek: 1 }
 };
 
 datasets.courses = {
   name: 'courses',
-  sql: 'SELECT * FROM DB2INST1.view_canvas_courses'
+  sql: 'SELECT * FROM DB2INST1.view_canvas_courses',
+  schedule: { hour: 10, minute: 0, dayOfWeek: [new schedule.Range(1,5)] }
 };
 
 datasets.staffUsers = {
   name: 'staff-users',
-  sql: 'SELECT * FROM DB2INST1.view_canvas_staff_users'
+  sql: 'SELECT * FROM DB2INST1.view_canvas_staff_users',
+  schedule: { hour: 10, minute: 15, dayOfWeek: [new schedule.Range(1,5)] }
 };
 
 datasets.studentUsers = {
   name: 'student-users',
-  sql: 'SELECT * FROM DB2INST1.view_canvas_student_users'
+  sql: 'SELECT * FROM DB2INST1.view_canvas_student_users',
+  schedule: { hour: 10, minute: 30, dayOfWeek: [new schedule.Range(1,5)] }
 };
 
 datasets.classTeachers = {
   name: 'class-teachers',
-  sql: 'SELECT * FROM DB2INST1.view_canvas_class_teachers'
+  sql: 'SELECT * FROM DB2INST1.view_canvas_class_teachers',
+  schedule: { hour: 10, minute: 45, dayOfWeek: [new schedule.Range(1,5)] }
 };
 
 datasets.classStudents = {
   name: 'class-students',
-  sql: 'SELECT * FROM DB2INST1.view_canvas_class_students'
+  sql: 'SELECT * FROM DB2INST1.view_canvas_class_students',
+  schedule: { hour: 11, minute: 0, dayOfWeek: [new schedule.Range(1,5)] }
 };
 
 module.exports = datasets;
