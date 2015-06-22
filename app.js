44784db0e2a0b146d8474adc1984850537d6d94a
Tim Douglas <neurotech@gmail.com>
2015-06-22 13:10:07 +1000

    Updated app.js to reflect new datasets structure

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var datasets = require('./datasets');
 var timetable = require('./lib/timetable');
 
 /* Schedule each dataset job based on it's `schedule` value in datasets.js */
+//timetable.job(datasets.subAccounts.name, datasets.subAccounts.sql, datasets.subAccounts.schedule);
 //timetable.job(datasets.terms.name, datasets.terms.sql, datasets.terms.schedule);
 //timetable.job(datasets.courses.name, datasets.courses.sql, datasets.courses.schedule);
-timetable.job(datasets.staffUsers.name, datasets.staffUsers.sql, datasets.staffUsers.schedule);
+//timetable.job(datasets.sections.name, datasets.sections.sql, datasets.sections.schedule);
+//timetable.job(datasets.staffUsers.name, datasets.staffUsers.sql, datasets.staffUsers.schedule);
 //timetable.job(datasets.studentUsers.name, datasets.studentUsers.sql, datasets.studentUsers.schedule);
-//timetable.job(datasets.classTeachers.name, datasets.classTeachers.sql, datasets.classTeachers.schedule);
-//timetable.job(datasets.classStudents.name, datasets.classStudents.sql, datasets.classStudents.schedule);
+//timetable.job(datasets.enrolments.name, datasets.enrolments.sql, datasets.enrolments.schedule);
