0084817ee098603761eac282a8de59c3edeb8c72
Tim Douglas <neurotech@gmail.com>
2015-06-22 12:53:05 +1000

    Added clean object to timetable

--- datasets.js
+++ datasets.js
@@ -13,31 +13,36 @@ datasets.terms = {
 datasets.courses = {
   name: 'courses',
   sql: 'SELECT * FROM DB2INST1.view_canvas_courses',
-  schedule: { hour: 10, minute: 0, dayOfWeek: [new schedule.Range(1,5)] }
+  //schedule: { hour: 10, minute: 0, dayOfWeek: [new schedule.Range(1,5)] }
+  schedule: { minute: [new schedule.Range(0,59)] }
 };
 
 datasets.staffUsers = {
   name: 'staff-users',
   sql: 'SELECT * FROM DB2INST1.view_canvas_staff_users',
-  schedule: { hour: 10, minute: 15, dayOfWeek: [new schedule.Range(1,5)] }
+  //schedule: { hour: 10, minute: 15, dayOfWeek: [new schedule.Range(1,5)] }
+  schedule: { minute: [new schedule.Range(0,59)] }
 };
 
 datasets.studentUsers = {
   name: 'student-users',
   sql: 'SELECT * FROM DB2INST1.view_canvas_student_users',
-  schedule: { hour: 10, minute: 30, dayOfWeek: [new schedule.Range(1,5)] }
+  //schedule: { hour: 10, minute: 30, dayOfWeek: [new schedule.Range(1,5)] }
+  schedule: { minute: [new schedule.Range(0,59)] }
 };
 
 datasets.classTeachers = {
   name: 'class-teachers',
   sql: 'SELECT * FROM DB2INST1.view_canvas_class_teachers',
-  schedule: { hour: 10, minute: 45, dayOfWeek: [new schedule.Range(1,5)] }
+  //schedule: { hour: 10, minute: 45, dayOfWeek: [new schedule.Range(1,5)] }
+  schedule: { minute: [new schedule.Range(0,59)] }
 };
 
 datasets.classStudents = {
   name: 'class-students',
   sql: 'SELECT * FROM DB2INST1.view_canvas_class_students',
-  schedule: { hour: 11, minute: 0, dayOfWeek: [new schedule.Range(1,5)] }
+  //schedule: { hour: 11, minute: 0, dayOfWeek: [new schedule.Range(1,5)] }
+  schedule: { minute: [new schedule.Range(0,59)] }
 };
 
 module.exports = datasets;
