90d8cee868713e80fc992c6f3b5f45fed41ca974
Tim Douglas <neurotech@gmail.com>
2015-06-22 12:01:53 +1000

    Testing terms timetable

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var timetable = require('./lib/timetable');
 
 /* Schedule each dataset job based on it's `schedule` value in datasets.js */
 timetable.job(datasets.terms.name, datasets.terms.sql, datasets.terms.schedule);
-timetable.job(datasets.courses.name, datasets.courses.sql, datasets.courses.schedule);
-timetable.job(datasets.staffUsers.name, datasets.staffUsers.sql, datasets.staffUsers.schedule);
-timetable.job(datasets.studentUsers.name, datasets.studentUsers.sql, datasets.studentUsers.schedule);
-timetable.job(datasets.classTeachers.name, datasets.classTeachers.sql, datasets.classTeachers.schedule);
-timetable.job(datasets.classStudents.name, datasets.classStudents.sql, datasets.classStudents.schedule);
+// timetable.job(datasets.courses.name, datasets.courses.sql, datasets.courses.schedule);
+// timetable.job(datasets.staffUsers.name, datasets.staffUsers.sql, datasets.staffUsers.schedule);
+// timetable.job(datasets.studentUsers.name, datasets.studentUsers.sql, datasets.studentUsers.schedule);
+// timetable.job(datasets.classTeachers.name, datasets.classTeachers.sql, datasets.classTeachers.schedule);
+// timetable.job(datasets.classStudents.name, datasets.classStudents.sql, datasets.classStudents.schedule);
