239f1d77be7fdecc94ed10657ee98cd9279af599
Tim Douglas <neurotech@gmail.com>
2015-06-22 12:59:17 +1000

    Updated datasets to match new order of operations

--- datasets.js
+++ datasets.js
@@ -3,10 +3,17 @@
 var schedule = require('node-schedule');
 var datasets = {};
 
+datasets.subAccounts = {
+  name: 'sub-accounts',
+  sql: 'SELECT * FROM DB2INST1.view_canvas_subaccounts',
+  //schedule: { hour: 18, minute: 0, dayOfWeek: 1 }
+  schedule: { minute: [new schedule.Range(0,59)] }
+};
+
 datasets.terms = {
   name: 'terms',
   sql: 'SELECT * FROM DB2INST1.view_canvas_terms',
-  //schedule: { hour: 18, minute: 0, dayOfWeek: 1 }
+  //schedule: { hour: 19, minute: 0, dayOfWeek: 1 }
   schedule: { minute: [new schedule.Range(0,59)] }
 };
 
@@ -17,30 +24,30 @@ datasets.courses = {
   schedule: { minute: [new schedule.Range(0,59)] }
 };
 
+datasets.sections = {
+  name: 'sections',
+  sql: 'SELECT * FROM DB2INST1.view_canvas_sections',
+  //schedule: { hour: 10, minute: 15, dayOfWeek: [new schedule.Range(1,5)] }
+  schedule: { minute: [new schedule.Range(0,59)] }
+};
+
 datasets.staffUsers = {
   name: 'staff-users',
   sql: 'SELECT * FROM DB2INST1.view_canvas_staff_users',
-  //schedule: { hour: 10, minute: 15, dayOfWeek: [new schedule.Range(1,5)] }
+  //schedule: { hour: 10, minute: 30, dayOfWeek: [new schedule.Range(1,5)] }
   schedule: { minute: [new schedule.Range(0,59)] }
 };
 
 datasets.studentUsers = {
   name: 'student-users',
   sql: 'SELECT * FROM DB2INST1.view_canvas_student_users',
-  //schedule: { hour: 10, minute: 30, dayOfWeek: [new schedule.Range(1,5)] }
-  schedule: { minute: [new schedule.Range(0,59)] }
-};
-
-datasets.classTeachers = {
-  name: 'class-teachers',
-  sql: 'SELECT * FROM DB2INST1.view_canvas_class_teachers',
   //schedule: { hour: 10, minute: 45, dayOfWeek: [new schedule.Range(1,5)] }
   schedule: { minute: [new schedule.Range(0,59)] }
 };
 
-datasets.classStudents = {
-  name: 'class-students',
-  sql: 'SELECT * FROM DB2INST1.view_canvas_class_students',
+datasets.enrolments = {
+  name: 'enrolments',
+  sql: 'SELECT * FROM DB2INST1.view_canvas_enrolments',
   //schedule: { hour: 11, minute: 0, dayOfWeek: [new schedule.Range(1,5)] }
   schedule: { minute: [new schedule.Range(0,59)] }
 };
