bd12bab9c2e8fe55ccdd2146c0e2cb86e7afaf50
Tim Douglas <neurotech@gmail.com>
2015-07-21 20:33:53 +1000

    Changed path to DB2 jar in config

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ config.edumate = {
 
 /* jdbc Initialisation Object */
 config.init = {
-  libpath: path.join(__dirname, '/drivers/db2jcc.jar'),
+  libpath: path.join(__dirname, '/node_modules/node-edumate/drivers/db2jcc.jar'),
   drivername: 'com.ibm.db2.jcc.DB2Driver',
   url: 'jdbc:' + 'db2://' + config.edumate.host + ':' + config.edumate.port + config.edumate.suffix + ':user=' + config.edumate.username + ';password=' + config.edumate.password + ';'
 };
