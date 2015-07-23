17ef71a79a88b158baf1bc966925f08f1f450d6a
Tim Douglas <neurotech@gmail.com>
2015-07-23 13:00:54 +1000

    Updated datasets.js to match keys required by timetable.job()

--- datasets.js
+++ datasets.js
@@ -4,43 +4,43 @@ var schedule = require('node-schedule');
 var datasets = {};
 
 datasets.subAccounts = {
-  name: 'sub-accounts',
+  dataset: 'sub-accounts',
   sql: 'SELECT * FROM DB2INST1.view_canvas_subaccounts',
   schedule: { hour: 18, minute: 0, dayOfWeek: 1 }
 };
 
 datasets.terms = {
-  name: 'terms',
+  dataset: 'terms',
   sql: 'SELECT * FROM DB2INST1.view_canvas_terms',
   schedule: { hour: 19, minute: 0, dayOfWeek: 1 }
 };
 
 datasets.courses = {
-  name: 'courses',
+  dataset: 'courses',
   sql: 'SELECT * FROM DB2INST1.view_canvas_courses',
   schedule: { hour: 10, minute: 0, dayOfWeek: [new schedule.Range(1, 5)] }
 };
 
 datasets.sections = {
-  name: 'sections',
+  dataset: 'sections',
   sql: 'SELECT * FROM DB2INST1.view_canvas_sections',
   schedule: { hour: 10, minute: 15, dayOfWeek: [new schedule.Range(1, 5)] }
 };
 
 datasets.staffUsers = {
-  name: 'staff-users',
+  dataset: 'staff-users',
   sql: 'SELECT * FROM DB2INST1.view_canvas_staff_users',
   schedule: { hour: 10, minute: 30, dayOfWeek: [new schedule.Range(1, 5)] }
 };
 
 datasets.studentUsers = {
-  name: 'student-users',
+  dataset: 'student-users',
   sql: 'SELECT * FROM DB2INST1.view_canvas_student_users',
   schedule: { hour: 10, minute: 45, dayOfWeek: [new schedule.Range(1, 5)] }
 };
 
 datasets.enrolments = {
-  name: 'enrolments',
+  dataset: 'enrolments',
   sql: 'SELECT * FROM DB2INST1.view_canvas_enrolments',
   schedule: { hour: 11, minute: 0, dayOfWeek: [new schedule.Range(1, 5)] }
 };
