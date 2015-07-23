6b4b0ba1b6be7d3a99ddd0ff8ee5e5b91c93c9c3
Tim Douglas <neurotech@gmail.com>
2015-07-23 13:01:19 +1000

    Added for loop to iterate over datasets and pass results to timetable module

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 'use strict';
 
+var moment = require('moment');
 var datasets = require('./datasets');
 var timetable = require('./lib/timetable');
 
 // Iterate over datasets and pass each one to timetable
+for (var key in datasets) {
+  if (datasets.hasOwnProperty(key)) {
+    console.log('Timetabled job: ' + datasets[key].dataset);
+
+    timetable.job(datasets[key])
+      .then(function (results) {
+        console.log(datasets[key].dataset + '.csv uploaded to Canvas.' + ' Import ID #' + results.id + ' started on ' + moment(results.created_at).format('dddd, MMMM Do YYYY [at] hh:mm:ss A.'));
+      }, function (error) {
+        console.error(error);
+      });
+  }
+}
