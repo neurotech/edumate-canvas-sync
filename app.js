0640076f7257e4a85b275c44c1121c0c3c5a26c2
Tim Douglas <neurotech@gmail.com>
2015-07-23 13:55:51 +1000

    Added spinUp IIFE to log a chalk-styled startup message

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 'use strict';
 
+var chalk = require('chalk');
 var moment = require('moment');
 var datasets = require('./datasets');
 var timetable = require('./lib/timetable');
 
+(function spinUp () {
+  var today = chalk.magenta(moment().format('DD/MM/YY'));
+  var now = chalk.yellow(moment().format('HH:mm:ss'));
+  console.log('Starting ' + chalk.green('edumate-canvas-sync') + ' on ' + today + ' at ' + now);
+  console.log(chalk.red('Canvas: ') + process.env.CANVAS_API_DOMAIN);
+  console.log(chalk.blue('Edumate: ') + process.env.EDUMATE_USERNAME + '@' + process.env.EDUMATE_HOST + ':' + process.env.EDUMATE_PORT + process.env.EDUMATE_PATH);
+}());
+
 // Iterate over datasets and pass each one to timetable
 for (var key in datasets) {
   if (datasets.hasOwnProperty(key)) {
-    console.log('Timetabled job: ' + datasets[key].dataset);
+    console.log('Scheduled job: ' + datasets[key].dataset);
 
     timetable.job(datasets[key])
       .then(function (results) {
