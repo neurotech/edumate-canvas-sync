6abce31de891f7d9ffe4132363b01d963737c3cb
Tim Douglas <neurotech@gmail.com>
2015-10-09 12:04:07 +1100

    Added forward slash to startup message

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var now = chalk.yellow(moment().format('HH:mm:ss'));
 
 console.log('Starting ' + chalk.green('edumate-canvas-sync') + ' on ' + today + ' at ' + now);
 console.log(chalk.red('Canvas: ') + process.env.CANVAS_API_DOMAIN);
-console.log(chalk.blue('Edumate: ') + process.env.EDUMATE_USERNAME + '@' + process.env.EDUMATE_HOST + ':' + process.env.EDUMATE_PORT + process.env.EDUMATE_PATH);
+console.log(chalk.blue('Edumate: ') + process.env.EDUMATE_USERNAME + '@' + process.env.EDUMATE_HOST + ':' + process.env.EDUMATE_PORT + '/' + process.env.EDUMATE_PATH);
 
 // Iterate over datasets and pass each one to timetable
 for (var key in datasets) {
