0a21f9c774e07f82f37cbc313b8cb0b41b6eab74
Tim Douglas <neurotech@gmail.com>
2015-10-09 11:59:18 +1100

    Removed spinUp IIFE

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ var moment = require('moment');
 var datasets = require('./datasets');
 var timetable = require('./lib/timetable');
 
-(function spinUp () {
-  var today = chalk.magenta(moment().format('DD/MM/YY'));
-  var now = chalk.yellow(moment().format('HH:mm:ss'));
-  console.log('Starting ' + chalk.green('edumate-canvas-sync') + ' on ' + today + ' at ' + now);
-  console.log(chalk.cyan('Status Endpoint: ') + chalk.underline('http://' + process.env.EDUMATE_CANVAS_SYNC_HOST + ':' + process.env.EDUMATE_CANVAS_SYNC_PORT));
-  console.log(chalk.red('Canvas: ') + process.env.CANVAS_API_DOMAIN);
-  console.log(chalk.blue('Edumate: ') + process.env.EDUMATE_USERNAME + '@' + process.env.EDUMATE_HOST + ':' + process.env.EDUMATE_PORT + process.env.EDUMATE_PATH);
-}());
+var today = chalk.magenta(moment().format('DD/MM/YY'));
+var now = chalk.yellow(moment().format('HH:mm:ss'));
+
+console.log('Starting ' + chalk.green('edumate-canvas-sync') + ' on ' + today + ' at ' + now);
+console.log(chalk.red('Canvas: ') + process.env.CANVAS_API_DOMAIN);
+console.log(chalk.blue('Edumate: ') + process.env.EDUMATE_USERNAME + '@' + process.env.EDUMATE_HOST + ':' + process.env.EDUMATE_PORT + process.env.EDUMATE_PATH);
 
 // Iterate over datasets and pass each one to timetable
 for (var key in datasets) {
@@ -26,3 +24,5 @@ for (var key in datasets) {
       });
   }
 }
+
+
