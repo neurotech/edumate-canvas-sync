5dcda4050aea71789cd7146cce15d069b9cfe69d
Tim Douglas <neurotech@gmail.com>
2015-10-09 12:00:04 +1100

    Cleaned up schedule objects in datasets.js

--- datasets.js
+++ datasets.js
@@ -3,46 +3,76 @@
 var schedule = require('node-schedule');
 var datasets = {};
 
+var businessDays = [new schedule.Range(1, 5)];
+
 datasets.subAccounts = {
   dataset: 'sub-accounts',
   sql: 'SELECT * FROM DB2INST1.view_canvas_subaccounts',
-  schedule: { hour: 18, minute: 0, dayOfWeek: 1 }
+  schedule: {
+    dayOfWeek: 1,
+    hour: 18,
+    minute: 0
+  }
 };
 
 datasets.terms = {
   dataset: 'terms',
   sql: 'SELECT * FROM DB2INST1.view_canvas_terms',
-  schedule: { hour: 19, minute: 0, dayOfWeek: 1 }
+  schedule: {
+    dayOfWeek: 1,
+    hour: 19,
+    minute: 0
+  }
 };
 
 datasets.courses = {
   dataset: 'courses',
   sql: 'SELECT * FROM DB2INST1.view_canvas_courses',
-  schedule: { hour: 10, minute: 0, dayOfWeek: [new schedule.Range(1, 5)] }
+  schedule: {
+    dayOfWeek: businessDays,
+    hour: 10,
+    minute: 0
+  }
 };
 
 datasets.sections = {
   dataset: 'sections',
   sql: 'SELECT * FROM DB2INST1.view_canvas_sections',
-  schedule: { hour: 10, minute: 15, dayOfWeek: [new schedule.Range(1, 5)] }
+  schedule: {
+    dayOfWeek: businessDays,
+    hour: 10,
+    minute: 15
+  }
 };
 
 datasets.staffUsers = {
   dataset: 'staff-users',
   sql: 'SELECT * FROM DB2INST1.view_canvas_staff_users',
-  schedule: { hour: 10, minute: 30, dayOfWeek: [new schedule.Range(1, 5)] }
+  schedule: {
+    dayOfWeek: businessDays,
+    hour: 10,
+    minute: 30
+  }
 };
 
 datasets.studentUsers = {
   dataset: 'student-users',
   sql: 'SELECT * FROM DB2INST1.view_canvas_student_users',
-  schedule: { hour: 10, minute: 45, dayOfWeek: [new schedule.Range(1, 5)] }
+  schedule: {
+    dayOfWeek: businessDays,
+    hour: 10,
+    minute: 45
+  }
 };
 
 datasets.enrolments = {
   dataset: 'enrolments',
   sql: 'SELECT * FROM DB2INST1.view_canvas_enrolments',
-  schedule: { hour: 11, minute: 0, dayOfWeek: [new schedule.Range(1, 5)] }
+  schedule: {
+    dayOfWeek: businessDays,
+    hour: 11,
+    minute: 0
+  }
 };
 
 module.exports = datasets;
