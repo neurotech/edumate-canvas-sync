791153c3b39793a006861345ad4db9fdd74415d8
Tim Douglas <neurotech@gmail.com>
2015-12-02 15:44:31 +1100

    Updated logger object structure to match convention

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
 'use strict';
 
-var moment = require('moment');
 var async = require('async');
 
 var config = require('./config');
 var datasets = require('./datasets');
 var timetable = require('./lib/timetable');
 
-var today = moment().format('DD/MM/YY');
-var now = moment().format('HH:mm:ss');
 var logger = config.logger;
 
 async.series([
   function (callback) {
-    logger.info('Starting edumate-canvas-sync on ' + today + ' at ' + now);
+    logger.info({
+      operation: 'Startup',
+      detail: {
+        message: 'Starting edumate-canvas-sync'
+      }
+    });
     setTimeout(function () { callback(null, 'starting'); }, 100);
   },
   function (callback) {
-    logger.info('Canvas Domain: ' + config.canvas.domain);
+    logger.info({
+      operation: 'Startup',
+      detail: {
+        message: 'Canvas Domain: ' + config.canvas.domain
+      }
+    });
     setTimeout(function () { callback(null, 'canvas domain'); }, 100);
   },
   function (callback) {
-    logger.info('Edumate Connection: ' + config.edumate.username + '@' + config.edumate.host + ':' + config.edumate.port + '/' + config.edumate.suffix);
+    logger.info({
+      operation: 'Startup',
+      detail: {
+        message: 'Edumate Connection: ' + config.edumate.username + '@' + config.edumate.host + ':' + config.edumate.port + '/' + config.edumate.suffix
+      }
+    });
     setTimeout(function () { callback(null, 'edumate info'); }, 100);
-  },
+  }
 ],
-function(err, results) {
+function (err, results) {
+  if (err) {
+    logger.error({
+      error: {
+        name: err.name,
+        message: err.message,
+        stack: err.stack
+      }
+    });
+  }
   // Iterate over datasets and pass each one to timetable
   for (var key in datasets) {
     if (datasets.hasOwnProperty(key)) {
@@ -33,7 +54,13 @@ function(err, results) {
 
       timetable.job(datasets[key])
         .then(function (results) {}, function (error) {
-          logger.error(error);
+          logger.error({
+            error: {
+              name: error.name,
+              message: error.message,
+              stack: error.stack
+            }
+          });
         });
     }
   }
