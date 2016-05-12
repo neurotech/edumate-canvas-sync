5c6d86eae0a28f50db6fa90ad6bb606f0ff1329b
Tim Douglas <neurotech@gmail.com>
2016-05-12 16:00:52 +1000

    Added hapi

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const relay = require('rollbar-relay');
 const config = require('./config');
 const datasets = require('./datasets');
 const timetable = require('./lib/timetable');
+const server = require('./lib/hapi');
 
 relay.info(`Starting edumate-canvas-sync | Canvas Domain: ${config.canvas.domain} | Edumate Connection: ${config.edumate.username}@${config.edumate.host}:${config.edumate.port}/${config.edumate.suffix}`);
 
+server.start(() => {
+  var announce = `hapi server up - version: ${server.version}`;
+  relay.info(announce);
+});
+
 // Iterate over datasets and pass each one to timetable
-for (var key in datasets) {
-  if (datasets.hasOwnProperty(key)) {
-    timetable.job(datasets[key])
+for (var set in datasets) {
+  if (datasets.hasOwnProperty(set)) {
+    timetable.job(datasets[set])
       .then((results) => {}, (error) => {
         relay.error(error);
       });
-    relay.info(`Scheduled Job: ${datasets[key].dataset}`);
+    relay.info(`Scheduled Job: ${datasets[set].dataset}`);
   }
 }
