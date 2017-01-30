15da1ee4d313ff979b8615c090ba8f60bbc9323c
Tim Douglas <neurotech@gmail.com>
2017-01-30 11:41:42 +1100

    Added LearnEd and Pastoral Care course enrolments

--- sql/view_canvas_evergreen_enrolments.sql
+++ sql/view_canvas_evergreen_enrolments.sql
@@ -18,9 +18,9 @@ WITH staff_evergreen_courses AS (
   UNION
   SELECT 'STAFFLEARNINGSUPPORT' AS "COURSE_ID", 'STAFF' AS "USER_TYPE" FROM SYSIBM.SYSDUMMY1
   UNION
-  SELECT 'PASTORALCARE' AS "COURSE_ID", 'STAFF' AS "USER_TYPE" FROM SYSIBM.SYSDUMMY1
-  UNION
   SELECT 'SURFACE-TRAINING' AS "COURSE_ID", 'STAFF' AS "USER_TYPE" FROM SYSIBM.SYSDUMMY1
+  UNION
+  SELECT 'LEARN-ED' AS "COURSE_ID", 'STAFF' AS "USER_TYPE" FROM SYSIBM.SYSDUMMY1
 ),
 
 student_evergreen_courses AS (
@@ -33,8 +33,8 @@ parent_evergreen_courses AS (
   SELECT 'LEARNINGRESOURCES' AS "COURSE_ID", 'PARENTS' AS "USER_TYPE" FROM SYSIBM.SYSDUMMY1
   UNION
   SELECT 'COMMUNITYRESOURCES' AS "COURSE_ID", 'PARENTS' AS "USER_TYPE" FROM SYSIBM.SYSDUMMY1
-  --UNION
-  --SELECT 'PASTORALCARE' AS "COURSE_ID", 'PARENTS' AS "USER_TYPE" FROM SYSIBM.SYSDUMMY1
+  UNION
+  SELECT 'PASTORALCARE-PARENTS' AS "COURSE_ID", 'PARENTS' AS "USER_TYPE" FROM SYSIBM.SYSDUMMY1
 ),
 
 combined_evergreen_courses AS (
@@ -118,10 +118,130 @@ student_careers_enrolments AS (
   WHERE form IN (10,11,12)
 ),
 
+student_learn_ed_enrolments AS (
+  SELECT
+    student.contact_id || '.' || student.student_number || '.' || student.student_id AS "USER_ID",
+    'LEARN-ED' AS "COURSE_ID",
+    'STUDENTS' AS "USER_TYPE"
+
+  FROM current_students
+  
+  INNER JOIN student ON student.student_id = current_students.student_id
+  
+  WHERE form = 10
+),
+
+student_pastoral_care_enrolments AS (
+  SELECT
+    student.contact_id || '.' || student.student_number || '.' || student.student_id AS "USER_ID",
+    (CASE
+      WHEN form = 7 THEN TO_CHAR((current date), 'YYYY') || '-PC-YR07'
+      WHEN form = 8 THEN TO_CHAR((current date), 'YYYY') || '-PC-YR08'
+      WHEN form = 9 THEN TO_CHAR((current date), 'YYYY') || '-PC-YR09'
+      WHEN form = 10 THEN TO_CHAR((current date), 'YYYY') || '-PC-YR10'
+      WHEN form = 11 THEN TO_CHAR((current date), 'YYYY') || '-PC-YR11'
+      WHEN form = 12 THEN TO_CHAR((current date), 'YYYY') || '-PC-YR12'
+    END) AS "COURSE_ID",
+    'STUDENTS' AS "USER_TYPE"
+  
+  FROM current_students
+  
+  INNER JOIN student ON student.student_id = current_students.student_id
+),
+
+pastoral_care_codes AS (
+  SELECT
+    TO_CHAR((current date), 'YYYY') || '-PC-YR' || (CASE WHEN form.short_name IN ('7','8','9') THEN '0' || form.short_name ELSE form.short_name END) AS COURSE_ID
+
+  FROM form
+),
+
+staff_ms3_pastoral_care_enrolments AS (
+  SELECT
+    current_staff.contact_id || '.' || staff.staff_number || '.' || staff.staff_id AS "USER_ID",
+    pastoral_care_codes.course_id,
+    'STAFF' AS "USER_TYPE"
+
+  FROM current_staff
+  
+  INNER JOIN staff ON staff.contact_id = current_staff.contact_id
+  LEFT JOIN teacher ON teacher.contact_id = current_staff.contact_id
+  LEFT JOIN class_teacher ON class_teacher.teacher_id = teacher.teacher_id
+  LEFT JOIN class ON class.class_id = class_teacher.class_id
+
+  CROSS JOIN pastoral_care_codes
+
+  WHERE
+    class_teacher.class_id IN (SELECT DISTINCT class_id FROM view_student_class_enrolment WHERE class_type_id = 2 AND academic_year = YEAR(current date))
+    AND
+    class.class LIKE '%MS3'
+),
+
+staff_non_ms3_pastoral_care_enrolments AS (
+  SELECT
+    current_staff.contact_id || '.' || staff.staff_number || '.' || staff.staff_id AS "USER_ID",
+    (CASE
+      WHEN class.class LIKE '%MS1' THEN '2017-PC-YR08'
+      WHEN class.class LIKE '%MS2' THEN '2017-PC-YR09'
+      WHEN class.class LIKE '%MS4' THEN '2017-PC-YR07'
+      WHEN class.class LIKE '%SS1' THEN '2017-PC-YR11'
+      WHEN class.class LIKE '%SS2' THEN '2017-PC-YR12'
+      WHEN class.class LIKE '%SS3' THEN '2017-PC-YR10'
+    END) AS "course_id",
+    'STAFF' AS "USER_TYPE"
+
+  FROM current_staff
+  
+  INNER JOIN staff ON staff.contact_id = current_staff.contact_id
+  LEFT JOIN teacher ON teacher.contact_id = current_staff.contact_id
+  LEFT JOIN class_teacher ON class_teacher.teacher_id = teacher.teacher_id
+  LEFT JOIN class ON class.class_id = class_teacher.class_id
+  
+  WHERE
+    class_teacher.class_id IN (SELECT DISTINCT class_id FROM view_student_class_enrolment WHERE class_type_id = 2 AND academic_year = YEAR(current date))
+    AND
+    class.class NOT LIKE '%MS3'
+),
+
+staff_pastoral_care_designer_enrolments AS (
+  SELECT
+    group_membership.contact_id || '.' || staff.staff_number || '.' || staff.staff_id AS "USER_ID",
+    pastoral_care_codes.course_id,
+    'STAFF' AS "USER_TYPE"
+
+  FROM group_membership
+
+  INNER JOIN staff ON staff.contact_id = group_membership.contact_id
+  CROSS JOIN pastoral_care_codes
+
+  WHERE
+    group_membership.groups_id IN (603,674)
+    AND
+    effective_start <= (current date)
+    AND
+    (effective_end IS null
+    OR
+    effective_end >= (current date))
+),
+
+staff_pastoral_care_enrolments AS (
+  SELECT * FROM staff_pastoral_care_designer_enrolments
+  UNION
+  SELECT * FROM staff_non_ms3_pastoral_care_enrolments
+  UNION
+  SELECT * FROM staff_ms3_pastoral_care_enrolments
+),
+
 combined_enrolments AS (
   SELECT * FROM parent_enrolments
   UNION
   SELECT * FROM student_careers_enrolments
+  UNION
+  SELECT * FROM student_learn_ed_enrolments
+  UNION
+  SELECT * FROM student_pastoral_care_enrolments
+  UNION
+  SELECT * FROM staff_pastoral_care_enrolments 
   UNION
   SELECT * FROM student_enrolments
   UNION
@@ -134,8 +254,9 @@ SELECT * FROM (
     null AS "ROOT_ACCOUNT",
     user_id,
     (CASE
-      WHEN combined_enrolments.course_id = 'PASTORALCARE' AND combined_enrolments.user_type = 'STAFF'
-      THEN 'DesignerEnrollment'
+      WHEN combined_enrolments.course_id = 'LEARN-ED' AND combined_enrolments.user_type = 'STAFF' THEN 'TA'
+      WHEN combined_enrolments.course_id LIKE '%-PC-%' AND combined_enrolments.user_id IN (SELECT DISTINCT user_id FROM staff_pastoral_care_designer_enrolments) THEN 'DesignerEnrollment'
+      WHEN combined_enrolments.course_id LIKE '%-PC-%' AND combined_enrolments.user_id NOT IN (SELECT DISTINCT user_id FROM staff_pastoral_care_designer_enrolments) AND combined_enrolments.user_type = 'STAFF' THEN 'teacher'
       ELSE 'student'
     END) AS "ROLE",
     null AS "SECTION_ID",
