12491d125081c15eaa988c8c381ebfb7e26c7206
Tim Douglas <neurotech@gmail.com>
2017-08-04 15:15:10 +1000

    Added environment-specific information to console log

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const datasets = require('./datasets');
 const timetable = require('./lib/timetable');
 const server = require('./lib/hapi');
 
-// Send a info event to Rollbar with relevant config information
+// Send a info event to Rollbar with environment-specific information
 relay.info(`Started edumate-canvas-sync.`, {
   canvas: config.canvas.domain,
   edumate: {
@@ -15,6 +15,13 @@ relay.info(`Started edumate-canvas-sync.`, {
     username: config.edumate.username
   }
 });
+
+// Log environment-specific information locally
+log(`Canvas Domain: ${config.canvas.domain}`);
+log(`Edumate Host: ${config.edumate.host}`);
+log(`Edumate Port: ${config.edumate.port}`);
+log(`Edumate Suffix: ${config.edumate.suffix}`);
+log(`Edumate Username: ${config.edumate.username}`);
 
 // Start the hapi server and log to console
 server.start(() => { log(`hapi server up - version: ${server.version}`); });
